f703e5322a56b23be799a49fa771999edaadd83f
Winston Chen <winstonchen.wm@gmail.com>
2021-03-27 10:45:39 -0400

    Second Commit

---
+++ planet-warrior-frontend/src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Earth from '../Earth/Earth';
+import Navbar from '../Navbar';
+import {HomeContainer} from './HomeElements'
+const Home = () => {
+    return (
+        <>
+        <HomeContainer>
+            <Navbar />
+        </HomeContainer>
+        </>
+    )
+}
+
+export default Home
---
+++ planet-warrior-frontend/src/globalStyles.js
@@ -0,0 +1,10 @@
+import {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        box-dizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: 'Kanit', sans-serif;
+    }
+`
