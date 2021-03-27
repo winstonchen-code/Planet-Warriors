f8d87d94e8b193d9bbd87db1c826563095490cab
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-03-27 15:26:20 -0400

    done_backend

--- planet-warrior-backend/app/controllers/user_tasks_controller.rb
+++ planet-warrior-backend/app/controllers/user_tasks_controller.rb
@@ -1,2 +1,22 @@
 class UserTasksController < ApplicationController
+
+    def create
+      user_task = UserTask.create(user_id: current_user.id, task_id: user_tasks_params[:task_id])
+      render json: user_task
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def user_tasks_params
+      params.require(:user_task).permit(:task_id)
+    end
+
 end
